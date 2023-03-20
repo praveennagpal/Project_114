0d3362f6ba4f31351c4d7e65b12923af8c4a192b
Praveen Nagpal <praveennagpal@users.noreply.github.com>
2023-03-20 12:06:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(640, 300);
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
